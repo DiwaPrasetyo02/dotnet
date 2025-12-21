e921ad97ed7c82fe47a0309f7b362732fa284012
Diwa Prasetyo <diwa@digits.id>
2025-12-21 22:06:55 +0700

    Feat: Add Swagger configuration for API versioning in Student application

---
+++ dotnet/Student/Data/ConfigureSwaggerOptions.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace WebAppMVC.Data
+{
+    public class ConfigureSwaggerOptions : IConfigureOptions<SwaggerGenOptions>
+    {
+        private readonly IApiVersionDescriptionProvider _provider;
+
+        public ConfigureSwaggerOptions(IApiVersionDescriptionProvider provider)
+        {
+            _provider = provider;
+        }
+
+        public void Configure(SwaggerGenOptions options)
+        {
+            foreach (var description in _provider.ApiVersionDescriptions)
+            {
+                options.SwaggerDoc(description.GroupName, new OpenApiInfo
+                {
+                    Title = "Student API",
+                    Version = description.ApiVersion.ToString(),
+                    Description = $"API untuk mengelola data Student - Versi {description.ApiVersion}"
+                });
+            }
+        }
+    }
+}
+
